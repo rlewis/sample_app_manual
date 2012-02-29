97b36ef684ab5aa6f9418a90390ae85eced52e74
Ryan Lewis <muckngrind@csu.fullerton.edu>
2012-02-29 12:40:03 -0800

    Added controller, route and view for about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,6 @@ class PagesController < ApplicationController
   def contact
   end
 
+  def about
+  end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -16,4 +17,10 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
